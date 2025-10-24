1291bd8caf3a3ec39f54da35041db7bf1dbe07d3
MarceloR05 <casillasmarcelo2005@gmail.com>
2025-10-24 15:06:23 -0600

    Reapply "FEAT-1: Inicio de Api"
    This reverts commit 0de8dac305538e713fa57b156fbdc54722a2a8eb.

---
+++ src/routes/users.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import {
+  getAllUsers,
+  getUserByEmail,
+  getUserByName,
+  getUserById,
+  postCrearUsuario,
+  actualizarUsuario,
+  eliminarUsuario,
+  filtrarUsuarios 
+} from '../services/usersServices.js';
+
+const router = express.Router();
+
+// Obtener todos los usuarios
+router.get('/', getAllUsers);
+
+// Buscar por email
+router.get('/buscarPorEmail/:email', getUserByEmail);
+
+// Buscar por nombre
+router.get('/buscarPorNombre/:nombre', getUserByName);
+
+// Crear usuario
+router.post('/', postCrearUsuario);
+
+// Actualizar usuario
+router.put('/:id_usuario', actualizarUsuario);
+
+// Eliminar usuario
+router.delete('/:id_usuario', eliminarUsuario);
+
+// Buscar por ID
+router.get('/buscarPorId/:id_usuario', getUserById);
+
+// Filtro dinámico (por query params)
+router.get('/filtrar', filtrarUsuarios);
+
+
+
+export default router;
