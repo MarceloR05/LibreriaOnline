de94c182710e2e6d7d1eaf0c36da7a91c4784df4
miguel <miguel.reyes1@catolica.edu.sv>
2025-10-25 21:42:12 -0600

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
+import carritoRoutes from './routes/carrito.js';
+import pedidoRoutes from './routes/pedido.js';
+import itemCarritoRoutes from './routes/itemCarrito.js';
+import direccionRoutes from './routes/direccion.js';
+import pagoRoutes from './routes/pago.js';
+
+
 import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
@@ -9,6 +16,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/carrito', carritoRoutes);
+app.use('/pedido', pedidoRoutes);
+app.use('/itemcarrito', itemCarritoRoutes);
+app.use('/direccion', direccionRoutes);
+app.use('/pago', pagoRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('API de Usuarios funcionando 🚀. Visita /users para ver los datos.');
